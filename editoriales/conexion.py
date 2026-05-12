f39e1e5758e3f8f1fd358a023bf9b86017f9021b
kevinpabonrodriguez4-png <kevinpabonrodriguez4@gmail.com>
2026-05-11 20:46:18 -0500

    Add files via upload

---
+++ editoriales/conexion.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+import mysql.connector
+
+mi_db = mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="",
+    database="biblioteca"
+)
+mi_cursor = mi_db.cursor()
